746839040d635d3571fa7757736e4bf1dbf13701
Edgar Padilla <MainComp@107-144-156-227.biz.spectrum.com>
2019-05-04 16:08:47 -0400

    testing setup, not finished

--- Project2ang/src/app/app.module.ts
+++ Project2ang/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
