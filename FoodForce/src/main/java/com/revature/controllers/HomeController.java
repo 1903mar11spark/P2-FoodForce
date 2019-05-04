4bb38612b1eee772cc3bfb62c719e8c6f7cbb168
william oleson <wnoleson@gmail.com>
2019-05-04 10:03:53 -0400

    fixed db, deleted enum, spring setup

---
+++ FoodForce/src/main/java/com/revature/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.revature.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public HomeController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
