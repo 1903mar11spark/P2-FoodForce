8d87c99b562aa4d4d85b995d2c4b2e4344c80c5d
TuckerBelton <48481353+TuckerBelton@users.noreply.github.com>
2019-05-07 09:14:04 -0400

    Update app.module.ts

--- Project2ang/src/app/app.module.ts
+++ Project2ang/src/app/app.module.ts
@@ -5,6 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 //Components
 import { AppComponent } from './app.component';
+
+import { LoginComponent } from './login/login.component';
+import { EmployeeMainComponent } from './employee-main/employee-main.component';
+import { MyInfoComponent } from './my-info/my-info.component';
+import { EmpPendingComponent } from './emp-pending/emp-pending.component';
+import { EmpAcceptedComponent } from './emp-accepted/emp-accepted.component';
+import { EmpMadeComponent } from './emp-made/emp-made.component';
+import { EmpCompleteComponent } from './emp-complete/emp-complete.component';
+import { CustomerMainComponent } from './customer-main/customer-main.component';
+import { CustOrderStatusComponent } from './cust-order-status/cust-order-status.component';
+import { CustOrderHistoryComponent } from './cust-order-history/cust-order-history.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { MenuPizzaComponent } from './menu-pizza/menu-pizza.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+
 import { LoginFormComponent } from './login-form/login-form.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -14,9 +29,24 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
+
+    LoginComponent,
+    EmployeeMainComponent,
+    MyInfoComponent,
+    EmpPendingComponent,
+    EmpAcceptedComponent,
+    EmpMadeComponent,
+    EmpCompleteComponent,
+    CustomerMainComponent,
+    CustOrderStatusComponent,
+    CustOrderHistoryComponent,
+    ContactUsComponent,
+    MenuPizzaComponent,
+    CheckOutComponent
+
     LoginFormComponent,
     FooterComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
