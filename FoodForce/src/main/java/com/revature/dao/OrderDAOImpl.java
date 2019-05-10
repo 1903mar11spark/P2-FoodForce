7fbc38cf723abac0c0ea37065ef56610bafa7b18
william oleson <wnoleson@gmail.com>
2019-05-10 13:00:46 -0400

    working

--- FoodForce/src/main/java/com/revature/dao/OrderDAOImpl.java
+++ FoodForce/src/main/java/com/revature/dao/OrderDAOImpl.java
@@ -3,14 +3,19 @@ package com.revature.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.revature.entities.Customer;
 import com.revature.entities.Order;
 
 
@@ -36,12 +41,30 @@ public class OrderDAOImpl implements OrderDAO {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Order> getCustomerOrders(int customerId) {
+		try {
+			
 		
-		List<Order> orders = new ArrayList<>();
 		
-		Session s = sessionFactory.getCurrentSession();
-		orders= s.createQuery("from Orders").getResultList();
-		return orders;
+		Session session = sessionFactory.getCurrentSession();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<Order> query = builder.createQuery(Order.class);
+		Root<Order> root = query.from(Order.class);
+        query.select(root).where(builder.equal(root.get("order.customerid"), customerId));
+        
+        Query<Order> q = session.createQuery(query);
+        Order o = q.getSingleResult();
+        return (List<Order>) o;	
+        
+		} catch (Exception e) {
+	         
+	         return null;
+		}
+
+		
+//		List<Order> orders = new ArrayList<>();
+//		Session s = sessionFactory.getCurrentSession();
+//		orders= s.createQuery("from Orders where order.customerid = ").getResultList();
+//		return orders;
 	}
 
 	@Override
