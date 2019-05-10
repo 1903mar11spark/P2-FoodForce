e1e8ed7fc99a0fb3857d33c0caf803f8550d798f
TuckerBelton <tuckerbelton@yahoo.com>
2019-05-10 11:30:09 -0400

    debugging

--- FoodForce/src/main/java/com/revature/controllers/FoodController.java
+++ FoodForce/src/main/java/com/revature/controllers/FoodController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Employee;
@@ -29,6 +30,7 @@ public class FoodController {
 
 	@CrossOrigin(origins = "http://localhost:4200/customer/menu/pizza")
 	@GetMapping("/")
+	@ResponseBody
 	public List<Food> home() {
 		List<Food> food = foodService.getAllFood();
 		System.out.println(food);
