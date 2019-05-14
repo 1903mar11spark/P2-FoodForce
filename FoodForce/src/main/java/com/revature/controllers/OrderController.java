64e015d630c988b78c9a4fd442b72cae34e6e19d
william oleson <wnoleson@gmail.com>
2019-05-14 11:55:19 -0400

    by type

--- FoodForce/src/main/java/com/revature/controllers/OrderController.java
+++ FoodForce/src/main/java/com/revature/controllers/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,9 @@ import com.revature.entities.Order;
 import com.revature.service.CustomerService;
 import com.revature.service.OrderService;
 
+
 @RestController //combines Controller and Request Body. Not sure if RequestBody should go here
+@CrossOrigin
 public class OrderController {
 	
 	@Autowired
